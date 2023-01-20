3e7c5296c3be69e9dc95c938c5a3904b622e6a5c
MH@Torture <m.hosein.hafezi.m@outlook.com>
2023-01-20 10:57:23 +0330

    .gitignore modified+ digikala_torture implemented with try except + readme eddited

--- digikala.com/digikala_torture.py
+++ digikala.com/digikala_torture.py
@@ -9,6 +9,30 @@ import pandas as pd
 import numpy as np
 import re
 import math
+import logging
+from logging.handlers import RotatingFileHandler
+
+dirname = os.path.dirname(__file__)
+
+my_handler = RotatingFileHandler(dirname+'\\report.txt', mode='w', maxBytes=5*1024, backupCount=0, encoding=None, delay=0)
+logging.basicConfig(format='[TIME: %(asctime)s] [DigiKala@Torture] %(levelname)s: %(message)s', level=logging.INFO,handlers=[my_handler]) #filename='Practices/log.log',
+LOGGER = logging.getLogger("digikala@torture")
+
+
+def syslogger(msg, sev = "info"):
+    if "debug" in sev :
+        LOGGER.debug(msg)        
+    elif "info" in sev :
+        LOGGER.info(msg)
+    elif "warning" in sev :
+        LOGGER.warning(msg)
+    elif "error" in sev :
+        LOGGER.error(msg)
+    elif "critical" in sev :
+        LOGGER.critical(msg)
+    else:
+        pass
+
 
 mapping = {
     '۰': '0',
@@ -38,26 +62,44 @@ opt.headless = True
 driver = webdriver.Chrome(options=opt) #options=opt
 driver.implicitly_wait(7)
 driver.maximize_window()
+
 driver.get("https://www.digikala.com/search/?has_selling_stock=1&q=flash%2064gb")
+syslogger("getting to https://www.digikala.com/search/?has_selling_stock=1&q=flash%2064gb")
 driver.implicitly_wait(10)
+syslogger("implicit wait set 10 sec!")
+
 def xpath_builder(get_option,get_value,get_state):
+    syslogger(f'xpath builder invoked! //{get_option}[@{get_value}="{get_state}"]')
     return f'//{get_option}[@{get_value}="{get_state}"]'
+try:
+    option,value,state = "h3","class","ellipsis-2 text-body2-strong color-700"
+    datas_name = driver.find_elements(By.XPATH,xpath_builder(option,value,state))
+except Exception as e :
+    syslogger(e)
 
-option,value,state = "h3","class","ellipsis-2 text-body2-strong color-700"
-datas_name = driver.find_elements(By.XPATH,xpath_builder(option,value,state))
+datas_name_text = []
 for data in datas_name:
-    print(data.text)
+    datas_name_text.append(data.text)
+syslogger(f"{datas_name_text}")
 
-option,value,state = "div","class","d-flex ai-center jc-end gap-1 color-700 color-400 text-h5 grow-1"
-datas_price = driver.find_elements(By.XPATH,xpath_builder(option,value,state))
-
-for ind , data in enumerate(datas_price):
-    print(data.text)
-
+try:
+    option,value,state = "div","class","d-flex ai-center jc-end gap-1 color-700 color-400 text-h5 grow-1"
+    datas_price = driver.find_elements(By.XPATH,xpath_builder(option,value,state))
+except Exception as e :
+    syslogger(e)
     
-j={   "number":[z for z in range(1,len(datas_name)+1)],
-      "name":[x.text.replace(",","") for x in datas_name],
-      "price":[y.text.replace(",","") for y in datas_price]}
+datas_price_text = []
+for ind , data in enumerate(datas_price):
+    datas_price_text.append(data.text)
+syslogger(f"{datas_price_text}")
+
+try:    
+    j={   "number":[z for z in range(1,len(datas_name)+1)],
+          "name":[x.text for x in datas_name],
+          "price":[y.text.replace(",","") for y in datas_price]}
+    syslogger(f"gathering data to save as csv and parse it as dataFrame! data as dict is :{j}")
+except Exception as e :
+    syslogger(e)
     
 converted_nums = []
 for key in j["price"]:
@@ -65,30 +107,34 @@ for key in j["price"]:
     converted_nums.append(string)
 j["price"] = converted_nums
 
-df = pd.DataFrame.from_dict(j)
-df.to_csv(os.path.dirname(__file__)+"\\prices.csv")
+try:
+    df = pd.DataFrame.from_dict(j)
+    df.to_csv(os.path.dirname(__file__)+"\\prices.csv",mode="w")
+except Exception as e :
+    syslogger(e)
+try:
+    dataf = pd.read_csv(os.path.dirname(__file__)+"\\prices.csv",index_col="number")
+    dataf.drop("Unnamed: 0",axis=1,inplace=True)
+    syslogger(f"{dataf.describe()}")
 
-dataf = pd.read_csv(os.path.dirname(__file__)+"\\prices.csv",index_col="number")
-dataf.drop("Unnamed: 0",axis=1,inplace=True)
-print(dataf.describe())
-print(dataf["price"].mean())
+    mean_price = dataf["price"].mean()
+    syslogger(f"mean is {mean_price}")
 
-mean_price = dataf["price"].mean()
+    f = []
+    for price in dataf["price"]:
+        diff = 1000000
+        f.append(int(price)-mean_price)
+        if abs(int(price)-mean_price) < diff:
+            diff = int(price)-mean_price
 
-f = []
+    for i,d in enumerate(f) :
+        if diff == d:
+            index_closest = i - 1
+        else:
+            pass
 
-for price in dataf["price"]:
-    diff = 1000000
-    f.append(int(price)-mean_price)
-    if abs(int(price)-mean_price) < diff:
-        diff = int(price)-mean_price
-
-for i,d in enumerate(f) :
-    if diff == d:
-        index_closest = i - 1
-    else:
-        pass
-
-print(f"most close price of product to mean calculated in this program is index : {index_closest} and name is : {datas_name[index_closest].text} so value is : {datas_price[index_closest].text}")   
+    syslogger(f"most close price of product to mean calculated in this program is index : {index_closest} and name is : {datas_name[index_closest].text} so value is : {datas_price[index_closest].text}")   
+except Exception as e :
+    syslogger(e)
 
 driver.quit()
