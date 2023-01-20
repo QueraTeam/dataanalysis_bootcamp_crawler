023a36d47f51b6e4bee134f9cbd0237c687fca70
mohammadsaleh40 <mohammadsalehmohammadsaleh@gmail.com>
2023-01-20 18:18:09 +0330

    استخراج داده‌های دیوار در صورتی که لینک بخواد index باشه.

--- divar.ir/scrape_divar_page.py
+++ divar.ir/scrape_divar_page.py
@@ -1,9 +1,11 @@
 # %%
 import json
 import requests
+from bs4 import BeautifulSoup
 import pandas as pd
 import numpy
 import time
+import os
 from functions import send_to_bale
 # %%
 url = "https://divar.ir/s/mashhad/buy-residential"
@@ -11,22 +13,53 @@ url = "https://divar.ir/s/mashhad/buy-residential"
 page = requests.get(url)
 
 # %%
+# اینجا کمک می‌کنه بفهمیم چه چیزایی استخراج شده
+# این طور که به نظر می‌رسه توی دیوار این صفحه که ذخیره می‌شه با مرورگر که بازش می‌کنیم یک لحظه نمایش می‌ده ولی یک هو همون جوری آفلاین که هست خطای ۴۰۴ نمایش می‌ده.
 f = open("what_scraped.html" , "w")
 f.write(page.text)
 f.close()
 
 # %%
-url = "https://api.divar.ir/v8/web-search/3/ROOT"
+def divar_page_to_DataFrame(page):
+    soup = BeautifulSoup(page.text , "html.parser")
+
+    mored_ha = soup.find_all("div"  , class_ = "post-card-item-af972 kt-col-6-bee95 kt-col-xxl-4-e9d46")
+
+    dict_ = {}
+    dict_["لینک"] = []
+    dict_["عنوان"] = []
+    dict_["قیمت"] = []
+    dict_["مکان"] = []
+
+    for x in mored_ha:
+        dict_["لینک"].append("https://divar.ir"+x.find("a" , href =True, class_ ="")["href"])
+        
+        dict_["عنوان"].append(x.find("h2",class_ = "kt-post-card__title").text)
+        
+        dict_["قیمت"].append(x.find("div",class_ = "kt-post-card__description").text)
+        dict_["مکان"].append(x.find("span",class_ = "kt-post-card__bottom-description kt-text-truncate").text.split("در")[-1])
+    df_ = pd.DataFrame(dict_).set_index("لینک")
+    #print(dict_["عنوان"])
+    return df_
+df_ = divar_page_to_DataFrame(page)
 # %%
-#                                                                                                      1674207963881798
-parametr = {"json_schema":{"category":{"value":"ROOT"},"query":"خانه","cities":["3"]},"last-post-date":1674207963889798}
-# %%
-page = requests.post(url , data = parametr)
+if "data.csv" not in os.listdir():
+    df = pd.DataFrame(columns=["لینک" , "عنوان" , "قیمت" , "مکان"]).set_index("لینک")
+else:
+    df = pd.read_csv("data.csv").set_index("لینک")
+
+df = pd.merge(df , df_ , how='outer' )
+df.to_csv("data.csv")
+
 
 # %%
-json.dumps(page.text)
+df
 # %%
-page
+df_
 # %%
-
+df = pd.DataFrame(columns=["لینک" , "عنوان" , "قیمت" , "مکان"]).set_index("لینک")
+df
+# %%
+df = pd.merge(df , df_ , how='outer' )
+df
 # %%
